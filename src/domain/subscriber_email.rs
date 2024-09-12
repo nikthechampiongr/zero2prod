e74496f379df26ea096c4a46325578138307e7e0
nikthechampiongr <nikvog4@gmail.com>
2024-09-13 02:38:31 +0300

    Update some testing dependencies

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -25,13 +25,16 @@ mod email_tests {
     use claims::assert_err;
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
 
     #[derive(Clone, Debug)]
     struct ValidEmailFixture(pub String);
 
     impl quickcheck::Arbitrary for ValidEmailFixture {
-        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
-            let email = SafeEmail().fake_with_rng(g);
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
             Self(email)
         }
     }
